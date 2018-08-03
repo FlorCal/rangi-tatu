d5992b54dcc9f56b72e8151bed9952fb06be26b7
Flor Calderon <maryf82@yahoo.com>
2018-08-02 22:13:14 -0700

    Sass is working with Webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './index.scss'
+
 class Container extends React.Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
       }
     ]
   },
