9a5098fcc7cf08f43dc65e534a2b1418b958bc6f
Flor Calderon <maryf82@yahoo.com>
2018-08-05 22:06:53 -0700

    Component file structure created

---
+++ src/Container/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Picker from '../Picker'
+
+class ContainerComponent extends React.Component {
+    render() {
+        return (
+            <div className='ContainerComponent'>
+                <Picker />
+            </div>
+        );
+    }
+}
+
+export default ContainerComponent
---
+++ src/Picker/index.js
@@ -0,0 +1,14 @@
+// libraries
+import React from 'react'
+
+
+class PickerComponent extends React.Component {
+    render() {
+        return (
+            <div className='PickerComponent'>
+            </div>
+        )
+    }
+}
+
+export default PickerComponent
