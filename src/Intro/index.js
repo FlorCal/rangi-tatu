c5bfa87fa2b21bd4e35177cc656e6b8a4ff0e5a0
Flor Calderon <maryf82@yahoo.com>
2018-08-10 23:03:18 -0700

    styling in IntroComponent completed.

--- src/Intro/index.js
+++ src/Intro/index.js
@@ -3,8 +3,15 @@ import React from 'react'
 
 class IntroComponent extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <div className='IntroComponent'>
+                <div className='title'>Rangi Tatu</div>
+                <div className='info'>
+                    Generate accessible and pleasing sets of 3 color palettes <br/>that are WCAG2 compliant.
+                    <div className='border'></div>
+                </div>
+                <button className='button' onClick={this.props.closeIntro}>Let&apos;s Go</button>
             </div>
         )
     }
