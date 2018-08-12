d1fe6af51c6d4453dc5b854a00da1ce83c9aaeb3
Flor Calderon <maryf82@yahoo.com>
2018-08-12 14:10:52 -0700

    styling for PickerIntroComponent completed!

--- src/PickerIntro/index.js
+++ src/PickerIntro/index.js
@@ -1,9 +1,19 @@
+//libraries
 import React from 'react'
 
+//assets
+import bowl from '../app/img/bowl.svg'
 class PickerIntroComponent extends React.Component {
     render() {
         return (
-            <div className='PickerIntroComponent'>
+            <div className='PickerIntroComponent' >
+                <div className='bowl'>
+                    <img src={bowl}/>
+                </div>
+                <div className='instruction'>
+                    Bwana, drop in that hex code so I can <br/>
+                    get to mixing these colors up
+                </div>
             </div>
         )
     }
