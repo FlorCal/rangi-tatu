e7ed54a1b5f32d67dfbbff826c019814b81beef1
Flor Calderon <maryf82@yahoo.com>
2018-08-15 13:13:08 -0700

    button in PickerComponent onClick to open LoadingComponent

---
+++ src/Loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class LoadingComponent extends React.Component {
+    render() {
+        return (
+            <div className='LoadingComponent'>
+            HELLO
+            </div>
+        )
+    }
+}
+
+export default LoadingComponent
