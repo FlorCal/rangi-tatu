46235b847935d1cf2901999fc74dcd47107dc7be
Flor Calderon <maryf82@yahoo.com>
2018-08-16 20:33:51 -0700

    styling of SelectorComponent completed.

--- src/Picker/Selector/index.js
+++ src/Picker/Selector/index.js
@@ -6,11 +6,11 @@ class SelectorComponent extends React.Component {
             <div className={'SelectorComponent ' + (this.props.passClass ? this.props.passClass : '') }>
                 <div className='title'>{this.props.title}</div>
                 <div className='wrapper'>
-                    {this.props.options.map((option, i) => <div
+                    {this.props.options.map((option, i) => <button
                         className={this.props.active === option ? 'active' : ''} key={i}
                         onClick={this.props.click.bind(this, option)}>
                         {option}
-                    </div> )}
+                    </button> )}
                 </div>
             </div>
         )
