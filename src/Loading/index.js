a1689f6b80142d6e786e345b656d50dc99d62b51
Flor Calderon <maryf82@yahoo.com>
2018-08-17 22:30:42 -0700

    styling of LoadingComponent completed

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 // assets
-// import bowl from './'
+import bowl from '../app/img/bowl.svg'
 
 class LoadingComponent extends React.Component {
     render() {
         return (
             <div className='LoadingComponent'>
-              LoadingComponent
+                <img className='bowl' src={bowl} />
+                <div className='stir'>Stir stir stir...</div>
             </div>
         )
     }
