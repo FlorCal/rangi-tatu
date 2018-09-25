3313ca9775828bf39173d5d1d5578945c5fddf94
Flor Calderon <maryf82@yahoo.com>
2018-09-25 16:59:32 -0700

    function discarding of non WCAG compliant schemes completed and working!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import convert from 'color-convert'
-
+import contrast from 'get-contrast'
 import './index.scss'
 import constants from '../constants'
 
@@ -63,13 +63,29 @@ class Renderer extends React.Component {
         return [firstComp, secondComp]
     }
 
+    isSchemeWcagCompliant(colorScheme) {
+        let wcagStandard = this.state.standard.split(' ')[1]
+        let passWcag = false
+
+        colorScheme.forEach(color => {
+            let hslbackground = `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+            let colorContrast = contrast.score(this.state.baseColor, hslbackground) // checks for AAA & AA contrast
+
+
+            if(colorContrast === wcagStandard || colorContrast === 'AAA') {
+                passWcag = true
+            }
+        })
+        return passWcag
+    }
 
 
     generateCombinations() {
         let newSchemesCombinations = []
         let baseHsl = convert.hex.hsl(this.state.baseColor.replace('#', ''))
         let hue = parseInt(this.state.hue)
-        let count = 0
+        let count1 = 0
+        let count2 = 0
 
         for(let d = 5; d <= 90; d += hue) { // primary hue step
             for(let dd = d + hue; dd <= 90; dd += hue){ // secondary hue step
@@ -80,22 +96,29 @@ class Renderer extends React.Component {
 
                 // FIXME: 15% shade actually stops at 90 and never reaches 100
                 // TODO: we need probably limits for shade
-                for(let l = constants.minShadeLightStep; l <= constants.maxShadeLightStep; l += shade){
-                    for(let s = constants.minSaturationStep; s <= constants.maxSaturationStep; s += 10){
-                        count += 1
-                        newSchemesCombinations.push([
+                for(let l = constants.minShadeLightStep; l <= constants.maxShadeLightStep; l += shade){ // shade/light step by 10
+                    for(let s = constants.minSaturationStep; s <= constants.maxSaturationStep; s += 10){ // saturation step by 10%
+                        count1 += 1
+
+                        let colorScheme = [
                             [complimentaryColors1[0], s, l],
                             [complimentaryColors2[0], s, l],
                             [complimentaryColors2[1], s, l],
                             [complimentaryColors1[1], s, l]
-                        ])
-                    }
+                        ]
+
+                        if(this.isSchemeWcagCompliant(colorScheme)) {
+                            count2 += 1
+                            newSchemesCombinations.push(colorScheme)
+                        }
+                    } // stepping through saturation
                 } // stepping through shade/lightness
 
             }
         }
 
-        console.log(count);
+        console.log(count1);
+        console.log(count2);
         this.setState({schemesCombinations: newSchemesCombinations})
     }
 
@@ -121,15 +144,15 @@ class Renderer extends React.Component {
     }
 
     changeStandard(val) {
-        this.setState({standard: val}, this.generateCombinations )
+        this.setState({standard: val})
     }
 
     changeHue(val) {
-        this.setState({hue: val}, this.generateCombinations )
+        this.setState({hue: val})
     }
 
     changeShade(val) {
-        this.setState({shade: val}, this.generateCombinations )
+        this.setState({shade: val})
     }
 
     createSchemes() {
@@ -149,7 +172,7 @@ class Renderer extends React.Component {
 
     baseColorChange(e) {
         let val = e.target.value.startsWith('#') ? e.target.value : '#' + e.target.value
-        this.setState({baseColor: val}, this.generateCombinations)
+        this.setState({baseColor: val})
     }
 
     render() {
