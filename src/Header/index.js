22bf4bb1677ca0bf4ec9d96006e2a2164d3b0dfe
Flor Calderon <maryf82@yahoo.com>
2018-09-30 14:30:29 -0700

    styling content depending on light or dark baseColor in HeaderComponent

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,35 +4,38 @@ import convert from 'color-convert'
 
 //assets
 import picker from '../app/img/picker.svg'
+import picker2 from '../app/img/picker2.svg'
 import question from '../app/img/question.svg'
+import question2 from '../app/img/question2.svg'
 
 class HeaderComponent extends React.Component {
 
-    isBaseColorLightOrDark() {
-        let textColor = '#333333'
-        if (this.props.baseColor && this.props.schemesCombinations.length > 0) {
+    isBaseColorDark() {
+        let darkColor = false
+        if (this.props.baseColor.length >= 3 && this.props.schemesCombinations.length > 0) {
             let color  = convert.hex.hsl(this.props.baseColor)
-            if (color[2] < 50) textColor = '#ffffff'
+            if (color[2] < 50) darkColor = true
         }
-        return textColor
+        return darkColor
     }
 
     render() {
-        // console.log(this.isBaseColorLightOrDark());
 
-        let baseColor = this.props.schemes ? this.props.baseColor : null
+        let baseColor = this.props.schemes && this.props.baseColor.length >= 3 ? this.props.baseColor : null
+
+        let textColor = this.isBaseColorDark() ? '#ffffff' : '#333333'
 
         return (
-            <div className='HeaderComponent' style={{color: this.isBaseColorLightOrDark(), background: baseColor}}>
+            <div className='HeaderComponent' style={{color: textColor, background: baseColor}}>
                 <div className='picker'>
-                    <img onClick={this.props.openPicker} src={picker} />
-                    <span onClick={this.props.openPicker}>Picker</span>
+                    <img onClick={this.props.openPicker} src={this.isBaseColorDark() ? picker2 : picker} />
+                    <span onClick={this.props.openPicker} style={{color: textColor}}>Picker</span>
                 </div>
                 <h1 className='title'>
                 Rangi Tatu
                 </h1>
                 <a className='q'>
-                    <img style={{fill: this.isBaseColorLightOrDark()}} onClick={this.props.openAbout} src={question}/>
+                    <img onClick={this.props.openAbout} src={this.isBaseColorDark() ? question2 : question}/>
                 </a>
             </div>
         )
