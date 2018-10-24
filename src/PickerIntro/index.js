c3349fb4e5584a031ae6efeb09b6bec2ae5e539b
Flor Calderon <maryf82@yahoo.com>
2018-10-24 10:49:01 -0700

    refactored PickerIntroComponent competed

--- src/PickerIntro/index.js
+++ src/PickerIntro/index.js
@@ -1,21 +1,12 @@
 //libraries
 import React from 'react'
 
-//assets
-import bowl from '../app/img/bowl.svg'
-
 
 class PickerIntroComponent extends React.Component {
     render() {
         return (
             <div className='PickerIntroComponent' >
-                <div className='bowl'>
-                    <img src={bowl}/>
-                </div>
-                <div className='instruction'>
-                    Bwana, drop in that hex code so I can
-                    get to mixing these colors up
-                </div>
+
             </div>
         )
     }
