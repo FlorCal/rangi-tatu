2e0ac7aaa67d1692aab36b279f960b7f6f2b82fe
Flor Calderon <maryf82@yahoo.com>
2018-10-26 10:49:14 -0700

    underlined WCAG in paragraphs set as links in WCAGInfoComponent

--- src/WCAGInfo/index.js
+++ src/WCAGInfo/index.js
@@ -28,21 +28,11 @@ class WCAGInfoComponent extends React.Component {
 
                     <div>What’s WCAG?</div>
                     <p>WCAG, is short for Web Content Accessibility Guidelines. What does that even mean you ask? They’re set of documents that explain how to make web content more accessible to people with disabilities</p>
-                    <p>WCAG AA refers to the lowest success criteria for color contrast ratios that allow for your website to be accessible. this means a contrast ratio of at least 4.5:1 of text or images of text.</p>
-                    <p>WCAG AAA is the highest success criteria, this means a contrast ratio of at least 7:1 of text or images of text.</p>
 
-                </div>
+                    <p>
+                        <span onClick={this.onWcagAAClick.bind(this)}>WCAG AA</span> refers to the lowest success criteria for color contrast ratios that allow for your website to be accessible. this means a contrast ratio of at least 4.5:1 of text or images of text.</p>
 
-                <div className='links'>
-
-                    <span>WCAG AA Link:</span>
-                    <div className='link' onClick={this.onWcagAAClick.bind(this)}>
-                    https://www.w3.org/TR/2016/NOTE-UNDERSTANDING-WCAG20-20161007/visual-audio-contrast-contrast.html
-                    </div>
-                    <span>WCAG AAA Link:</span>
-                    <div className='link' onClick={this.onWcagAAAClick.bind(this)}>
-                    https://www.w3.org/TR/2016/NOTE-UNDERSTANDING-WCAG20-20161007/visual-audio-contrast7.html
-                    </div>
+                    <p><span onClick={this.onWcagAAAClick.bind(this)}>WCAG AAA</span> is the highest success criteria, this means a contrast ratio of at least 7:1 of text or images of text.</p>
 
                 </div>
 
