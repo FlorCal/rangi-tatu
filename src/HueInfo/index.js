52ba29711ec2bfc314a61cdf750eae1c82356407
Flor Calderon <maryf82@yahoo.com>
2018-11-01 19:20:41 -0700

    content implemented in HueInfoComponent

--- src/HueInfo/index.js
+++ src/HueInfo/index.js
@@ -3,6 +3,11 @@ import React from 'react'
 //components
 import Button from '../Button'
 
+//assets
+import hue10 from '../app/img/hue10.svg'
+import hue15 from '../app/img/hue15.svg'
+import hue20 from '../app/img/hue20.svg'
+
 class HueInfoComponent extends React.Component {
 
 
@@ -17,13 +22,17 @@ class HueInfoComponent extends React.Component {
 
                 <div className='about'>
 
-                    <div></div>
-                    <p></p>
+                    <div>What’s Hue Step?</div>
 
-                    <p></p>
+                    <p>Hue is also known as color or the position on a color wheel. Hue step is the degree of separation between different colors on the wheel. So the wider the hue step, the larger the variance of colors but lower the number of combinations.</p>
 
-                    <p></p>
+                    <p>Test it out below with our little tool to get a better idea.</p>
 
+                    <div>
+                        <div className='wrapper'>
+                        </div>
+                        <img src={hue10}/>
+                    </div>
                 </div>
 
             </div>
