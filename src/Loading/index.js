5713799c041f6ab953f80e66bc9cae7467572e17
Flor Calderon <maryf82@yahoo.com>
2018-11-02 22:00:36 -0700

    background and content styling refactored in LoadingComponent

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import convert from 'color-convert'
+import bowlWhite from '../app/img/bowl-white.svg'
 
 // assets
 import bowl from '../app/img/bowl.svg'
 
 class LoadingComponent extends React.Component {
+
+    isBaseColorDark() {
+        let darkColor = false
+        if (this.props.baseColor.length >= 3 && this.props.schemesCombinations.length >= 0) {
+            let color  = convert.hex.hsl(this.props.baseColor)
+            if (color[2] < 50) darkColor = true
+        }
+        return darkColor
+    }
+
     render() {
+        console.log(this.props);
+
+        let baseColor = this.props.schemes && this.props.baseColor.length >= 3 ? this.props.baseColor : null
+
+        let textColor = this.isBaseColorDark() ? '#ffffff' : '#333333'
+
         return (
-            <div className='LoadingComponent'>
-                <img className='bowl' src={bowl} />
+            <div className='LoadingComponent' style={{background: baseColor, color: textColor}}>
+                <img className='bowl' src={this.isBaseColorDark() ? bowlWhite : bowl} />
                 <div className='stir'>Stir stir stir...</div>
             </div>
         )
