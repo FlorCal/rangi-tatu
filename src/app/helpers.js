9ca8579a479e515d1de530623c5ac33866c084a8
Flor Calderon <maryf82@yahoo.com>
2019-01-28 23:38:14 -0800

    isBaseColorDark function now checking for contrast between baseColor and white to determine page content color

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -1,11 +1,15 @@
-import convert from 'color-convert'
+import contrast from 'get-contrast'
 
 export default {
+
     isBaseColorDark: (props) => {
         let darkColor = false
+
         if (props.baseColor.length >= 3 && props.schemesCombinations.length >= 0) {
-            let color  = convert.hex.hsl(props.baseColor)
-            if (color[2] < 50) darkColor = true
+
+            let colorContrast = contrast.score(props.baseColor, '#fff')
+
+            if (colorContrast === 'AA') darkColor = true
         }
         return darkColor
     }
