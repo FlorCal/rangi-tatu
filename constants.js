5baefdb48829860c52cc003b180ea3d9584613d3
Flor Calderon <maryf82@yahoo.com>
2019-02-06 17:00:00 -0800

    images updated and moved to constants

--- constants.js
+++ constants.js
@@ -1,3 +1,14 @@
+import hue10 from './src/app/img/hue10.png'
+import hue15 from './src/app/img/hue15.png'
+import hue20 from './src/app/img/hue20.png'
+import saturation10 from './src/app/img/saturation10.png'
+import saturation15 from './src/app/img/saturation15.png'
+import saturation20 from './src/app/img/saturation20.png'
+import shade10 from './src/app/img/shade10.png'
+import shade15 from './src/app/img/shade15.png'
+import shade20 from './src/app/img/shade20.png'
+
+
 export default {
     minShadeLightStep: 10,
     maxShadeLightStep: 90,
@@ -7,5 +18,10 @@ export default {
         hueOptions: ['10°', '15°', '20°'],
         saturationOptions: ['10%', '15%', '20%'],
         shadeOptions: ['10%', '15%', '20%']
+    },
+    imageOptions: {
+        hueImages: [hue10, hue15, hue20],
+        saturationImages: [saturation10, saturation15, saturation20],
+        shadeImages: [shade10, shade15, shade20]
     }
 }
